5aeb5a551108f33709bc380d8556eae0acbc795b
Dave Kinkead <dave@kinkead.com.au>
2015-10-11 13:21:06 +1000

    Add more specs to implement

--- specs/ristretto_spec.rb
+++ specs/ristretto_spec.rb
@@ -6,14 +6,24 @@ describe Ristretto do
   let(:indented_codeblocks) { Ristretto.parse('specs/markdown/indented_codeblocks.md') }
 
   describe "#execute" do
-    it "executes parsed ruby code" do
+    it "executes simple ruby code" do
       Ristretto.execute(indented_codeblocks).must_equal "Hello, Ristretto!"
     end
+
+    it "can cope with unbound methods"
   end
 
   describe "#parse" do
     it "parses markdown indented codeblocks" do
       indented_codeblocks.must_equal "welcome = \"Hello, Ristretto!\"\n# comments\nwelcome"
     end
+
+    it "ignores indented lists"
+
+    it "ignores shell commands prefixed with $"
+
+    it "groks tabbed indents"
+
+    it "parses backticked codeblocks"
   end
 end
